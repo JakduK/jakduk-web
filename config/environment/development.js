769bceeac70b50299018ebe31489840ea780e906
silverprize <silverprizelee@gmail.com>
2016-07-31 16:26:59 +0900

    add internalApiServerUrl for dev

--- config/environment/development.js
+++ config/environment/development.js
@@ -1,5 +1,6 @@
 module.exports = {
-  apiServerUrl: 'https://staging.jakduk.com:8080/api',
+  internalApiServerUrl: 'https://staging.jakduk.com:8080/api',
+  apiServerUrl: 'https://staging.jakduk.com/api',
   thumbnailServerUrl: 'https://staging.jakduk.com:8080',
   origin: 'https://staging.jakduk.com',
   facebook: {
