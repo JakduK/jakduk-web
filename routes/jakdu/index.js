4ecd5886d0513e4e05f713b393dba9003197cc57
silverprize <silverprizelee@gmail.com>
2016-08-06 19:27:30 +0900

    clear jsp

--- routes/jakdu/index.js
+++ routes/jakdu/index.js
@@ -7,17 +7,29 @@ module.exports.setup = function (app) {
   var router = express.Router();
 
   router.get('/', function (req, res) {
-    res.render('error/coming_soon', {
+    res.render('jakdu/schedule', {
       title: [
+        i18n.__('jakdu.schedule'),
         i18n.__('jakdu'),
         i18n.__('common.jakduk')
       ]
     });
   });
 
-  router.get('/schedule', function (req, res) {
-    res.render('error/coming_soon', {
+  router.get('/:id', function (req, res) {
+    res.render('jakdu/view', {
       title: [
+        i18n.__('jakdu.view'),
+        i18n.__('jakdu'),
+        i18n.__('common.jakduk')
+      ]
+    });
+  });
+
+  router.get('/write', function (req, res) {
+    res.render('jakdu/write', {
+      title: [
+        i18n.__('jakdu.write'),
         i18n.__('jakdu'),
         i18n.__('common.jakduk')
       ]
