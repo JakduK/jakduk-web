7efb9a5baed5cbb8e6a34f916b8e0e8a34230a9e
silverprize <silverprizelee@gmail.com>
2016-10-08 09:01:08 +0900

    ajax 상대경로 사용

--- config/environment/development.js
+++ config/environment/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   origin: process.env.ORIGIN || 'http://localhost:3000',
-  apiServerUrl: process.env.API_SERVER || 'http://localhost:3000/api',
+  apiServerUrl: process.env.API_SERVER || '/api',
   internalApiServerUrl: process.env.INTERNAL_API_SERVER || 'http://localhost:8080/api',
   thumbnailServerUrl: process.env.THUMBNAIL_SERVER || 'http://localhost:8080'
 };
--- config/environment/production.js
+++ config/environment/production.js
@@ -1,13 +1,13 @@
 module.exports = {
   staging: {
     origin: process.env.ORIGIN || 'https://staging.jakduk.com',
-    apiServerUrl: process.env.API_SERVER || 'https://staging.jakduk.com/api',
+    apiServerUrl: process.env.API_SERVER || '/api',
     internalApiServerUrl: process.env.INTERNAL_API_SERVER || 'https://staging.jakduk.com:8080/api',
     thumbnailServerUrl: process.env.THUMBNAIL_SERVER || 'https://staging.jakduk.com:8080'
   },
   prod: {
     origin: process.env.ORIGIN || 'https://jakduk.com',
-    apiServerUrl: process.env.API_SERVER || 'https://jakduk.com/api',
+    apiServerUrl: process.env.API_SERVER || '/api',
     internalApiServerUrl: process.env.INTERNAL_API_SERVER || 'https://api.jakduk.com/api',
     thumbnailServerUrl: process.env.THUMBNAIL_SERVER || 'https://api.jakduk.com',
     gaAccount: 'UA-59051176-1'
