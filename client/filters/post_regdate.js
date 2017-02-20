78422374e67d8ce9c227e9b453fc585cd99e2aca
silverprize <silverprizelee@gmail.com>
2017-02-20 23:20:38 +0900

    spa로 구조 변경 진행 검증 - home 상대시간 표시 적용

--- client/filters/post_regdate.js
+++ client/filters/post_regdate.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 
 export default function (id, format) {
-  return moment(new Date(parseInt(id.slice(0, 8), 16) * 1000)).format(format);
+  const timestamp = parseInt(id.slice(0, 8), 16) * 1000;
+  return Date.now() - (1000 * 60 * 60 * 24 * 7) > timestamp ? moment(timestamp).format(format) : moment(timestamp).fromNow();
 }
