77126c5874dc37f8ef55fe21cc9b41afc0654c5c
silverprize <silverprizelee@gmail.com>
2017-02-21 20:09:47 +0900

    ie polyfill

--- server/middlewares/default_context.js
+++ server/middlewares/default_context.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var querystring = require('querystring');
-var Util = require('../helpers/jakduk_util');
-var config = require('../config/environment');
-var _ = require('lodash');
-var i18n = require('i18n');
+const querystring = require('querystring');
+const Util = require('../helpers/jakduk_util');
+const config = require('../config/environment');
+const _ = require('lodash');
+const i18n = require('i18n');
 
 module.exports = function () {
   return function defaultContext(req, res, next) {
@@ -21,19 +21,20 @@ module.exports = function () {
         req.isAdmin = res.locals.isAdmin;
       }
 
-      _.merge(res.locals, {
-        redir: '?redir=' + querystring.escape(req.url),
+      _.extend(res.locals, {
+        redir: `?redir=${querystring.escape(req.url)}`,
         layout: 'layout',
         userInfo: req.userInfo,
         isAuthenticated: req.isAuthenticated,
+        isIE: /trident|msie/i.test(req.headers['user-agent']),
         meta: {
           twitter: {
             card: 'summary'
           },
           og: {
             description: i18n.__('about.jakduk'),
-            url: config.origin + req.path,
-            image: config.origin + '/jakduk/img/logo_256.png',
+            url: `${config.origin}req.path`,
+            image: `${config.origin}/jakduk/img/logo_256.png`,
             type: 'website'
           },
           facebook: {
