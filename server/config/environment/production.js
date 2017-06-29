27b0ab07b198a45524c0e8d1e46340f1a1a94744
silverprize <silverprizelee@gmail.com>
2017-06-29 09:39:52 +0900

    https 복원

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,17 +1,17 @@
 module.exports = {
   staging: {
-    origin: process.env.ORIGIN || 'http://staging.jakduk.com',
+    origin: process.env.ORIGIN || 'https://staging.jakduk.com',
     apiServerUrl: process.env.API_SERVER || '/api',
-    internalApiServerUrl: process.env.INTERNAL_API_SERVER || 'http://staging.jakduk.com:8080/api',
-    thumbnailServerUrl: process.env.THUMBNAIL_SERVER || 'http://staging.jakduk.com:8080',
+    internalApiServerUrl: process.env.INTERNAL_API_SERVER || 'https://staging.jakduk.com:8080/api',
+    thumbnailServerUrl: process.env.THUMBNAIL_SERVER || 'https://staging.jakduk.com:8080',
     secureCookie: false,
     debug: true
   },
   prod: {
-    origin: process.env.ORIGIN || 'http://jakduk.com',
+    origin: process.env.ORIGIN || 'https://jakduk.com',
     apiServerUrl: process.env.API_SERVER || '/api',
-    internalApiServerUrl: process.env.INTERNAL_API_SERVER || 'http://api.jakduk.com/api',
-    thumbnailServerUrl: process.env.THUMBNAIL_SERVER || 'http://api.jakduk.com',
+    internalApiServerUrl: process.env.INTERNAL_API_SERVER || 'https://api.jakduk.com/api',
+    thumbnailServerUrl: process.env.THUMBNAIL_SERVER || 'https://api.jakduk.com',
     gaAccount: 'UA-59051176-1',
     secureCookie: false,
     debug: false
