3baf5356ca7524b2b0bda517e2755f8598ab142a
silverprize <silverprizelee@gmail.com>
2018-05-13 00:30:45 +0900

    Notification integration 이전 - Slack -> Telegram

---
+++ server/helpers/telegram_notifier.js
@@ -0,0 +1,28 @@
+const debug = require('debug')('jakduk-web:telegram');
+const restler = require('restler');
+const clor = require('clor');
+
+module.exports = function (options) {
+  const apiBase = `https://api.telegram.org/bot${options.token}`;
+  return function (message) {
+    const content = [
+      `🔔 <strong>New ${message.isPost ? 'Post' : 'Comment'}!</strong> - by ${message.author}`,
+      `<a href="${message.link}">${message.link}</a>`,
+      '',
+      `${message.text}`
+    ];
+
+    restler.postJson(`${apiBase}/sendMessage`, {
+      chat_id: options.chat_id,
+      text: content.join('\n'),
+      parse_mode: 'html'
+    }).on('complete', (data, response) => {
+      if (response) {
+        debug('%o', response.statusCode, JSON.stringify(message), JSON.stringify(data));
+      }
+    }).on('error', err => {
+      debug('%o', err.code, clor.red(err.message), err.stack);
+    });
+  };
+};
+
